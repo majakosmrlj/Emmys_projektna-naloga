b7a8edf029915305255216a9750910e71d3d66ee
majakosmrlj <mk37033@student.uni-lj.si>
2024-08-12 18:57:45 +0200

    funkcija za poiskati namonirance

---
+++ poisci_nominirance.py
@@ -0,0 +1,11 @@
+
+import re
+
+with open("emmys.html") as dat:
+    besedilo = dat.read()
+    niz = r'<h5>(?P<nominacija>.+?)</h5>'
+    nominacije = []
+    for najdba in re.finditer(niz, besedilo):
+        nominacije.append(najdba["nominacija"])
+        
+    print(nominacije, len(nominacije))
