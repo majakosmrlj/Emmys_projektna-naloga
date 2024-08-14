0c0802d4ccb6a3c97ed10e21dad0cb93e17744b4
majakosmrlj <mk37033@student.uni-lj.si>
2024-08-14 16:42:15 +0200

    nova koda

--- poisci_nominirance.py
+++ poisci_nominirance.py
@@ -1,12 +1,13 @@
 
-
 import re
 
 with open("emmys.html", encoding ="utf8") as dat:
     besedilo = dat.read()
-    niz = r'<h5>(?P<nominacija>.+?)</h5>'
+    niz = (r'<h5>(?P<nominacija>.+?)\B-(?P<leto>.+?)</h5>'
+           r'<div class="label_16 uppercase">(?P<rezultat>.+?)</div>')
     nominacije = []
     for najdba in re.finditer(niz, besedilo):
-        nominacije.append(najdba["nominacija"])
+        nominacije.append((najdba["nominacija"][:-1], najdba["leto"][1:], najdba["rezultat"]))
         
+
 print(nominacije, len(nominacije))
